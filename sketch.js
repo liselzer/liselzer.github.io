88af4584de718859be98b772c3271cfe34564389
liselzer <lilly.jselzer@gmail.com>
2024-08-08 09:45:36 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function setup() {
     ];
 
     pacman1 = new PacMan(0, 0, 2, 0, color(249, 201, 0), pathPoints, 0);
-    pacman2 = new PacMan(width, height, -2, 0, color(0, 191, 255), pathPoints, 2);
+    pacman2 = new PacMan(200, 200, -2, 0, color(0, 191, 255), pathPoints, 2);
 }
 
 function draw() {
