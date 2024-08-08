5aad82e7ad4424cef39514d392d247c7109be767
liselzer <lilly.jselzer@gmail.com>
2024-08-07 21:16:36 -0700

    updates

--- sketch.js
+++ sketch.js
@@ -3,39 +3,50 @@ let pacman1, pacman2;
 function setup() {
     let canvas = createCanvas(340, 340);
     canvas.parent(document.querySelector('.canvas-container'));
-    pacman1 = new PacMan(0, 10, 2, 0);
-    pacman2 = new PacMan(width, height - 10, -2, 0);
+    pacman1 = new PacMan(50, height / 2, 2, 0, color(249, 201, 0));
+    pacman2 = new PacMan(width - 50, height / 2, -2, 0, color(0, 191, 255));
 }
 
 function draw() {
     clear();
-    pacman1.move();
+    pacman1.moveTowards(pacman2);
     pacman1.display();
-    pacman2.move();
+    pacman2.moveTowards(pacman1);
     pacman2.display();
 }
 
 class PacMan {
-    constructor(x, y, dx, dy) {
+    constructor(x, y, dx, dy, c) {
         this.x = x;
         this.y = y;
         this.dx = dx;
         this.dy = dy;
         this.size = 30;
+        this.color = c;
         this.mouthOpen = true;
+        this.counter = 0;
+        this.mouthSpeed = 20; // Control mouth speed
     }
 
-    move() {
+    moveTowards(target) {
+        let angle = atan2(target.y - this.y, target.x - this.x);
+        this.dx = cos(angle) * 2;
+        this.dy = sin(angle) * 2;
         this.x += this.dx;
         this.y += this.dy;
-        if (this.x < 0 || this.x > width) this.dx *= -1;
-        if (this.y < 0 || this.y > height) this.dy *= -1;
     }
 
     display() {
-        fill(249, 201, 0);
+        fill(this.color);
+        noStroke();
         let angle = this.mouthOpen ? QUARTER_PI : 0;
         arc(this.x, this.y, this.size, this.size, angle, TWO_PI - angle);
-        this.mouthOpen = !this.mouthOpen;
+
+        // Control mouth open/close speed
+        this.counter++;
+        if (this.counter > this.mouthSpeed) {
+            this.mouthOpen = !this.mouthOpen;
+            this.counter = 0;
+        }
     }
 }
